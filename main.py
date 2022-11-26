840f7dd5689309459d3ec45e73e7511a9edd1744
HAlGhanim <imzaji98@gmail.com>
2022-11-26 20:05:48 +0300

    cw2

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 # write your code here
+# الجزء الاول
+# my_name = input("What is your name?")
+# my_age = input("How old are you?")
+# print(f"my name is {my_name} and I am {my_age} years old")
+
+# الجزء الثاني
+# y = int(input("Type the first number"))
+# z = int(input("Type the second number"))
+# operation = input("Pick an operator +, -, *, /")
+
+# if operation == "+":
+#     print(y + z)
+# elif operation == "-":
+#     print(y-z)
+# elif operation == "*":
+#     print(y*z)
+# elif operation == "/":
+#     print(y/z)
+# else:
+#     print("The operation is not valid")
+
+# الجزء الثالث
+# num = input("Enter a number from 1 to 12: ")
+# noun = input("Enter a noun (plural):")
+# name = input("Enter a name:")
+# sen = input("Enter any sentence:")
+# verb = input("Enter a verb:")
+
+# print(f"The story goes... \n \n It was {num} o'clock when I heard a knock at the door.\n I opened the door and there was a box full of {noun} with a note saying 'From Mr. {name}'.\n Just as I closed the door I heard a scream '{sen}'\n I froze in place and all I could do was {verb}")
+
